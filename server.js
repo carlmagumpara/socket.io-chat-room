e6664d55ff57c1165eb0099e49d03b7bc8c002c6
carlmagumpara <carlmagumpara@e-lawsolutions.com>
2019-12-04 15:12:17 +0800

    fix

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+const path = require('path');
+const app = express();
 const port = process.env.PORT || 5000;
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
